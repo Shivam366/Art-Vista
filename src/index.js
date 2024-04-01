57386dc690aaa93113530bb864a252278a93ba91
Shivam366 <chaudharyshivam5010@gmail.com>
2024-04-01 07:06:58 -0400

    Created the category pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ShopContextProvider from "./Context/ShopContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ShopContextProvider>
+    <App />
+  </ShopContextProvider>
+);
